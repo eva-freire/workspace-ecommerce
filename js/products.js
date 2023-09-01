d414ea9e590fa1fac11d59ea64b09e4e67190287
eva-freire <freiretrecueva@gmail.com>
2023-09-01 00:11:42 -0300

    modificacion del fetch, agrego funcion de mostrar data en pantalla, filtrar rango y me falta mostrar los rangos devueltos en pantalla

--- js/products.js
+++ js/products.js
@@ -5,13 +5,119 @@ const CAT_PRODUCTS = BASE_API + cat_id + '.json';
 //selecciono el localstorage, creo una BASE_URL y a DATA_AUTOS le concateno catID y ".json"
 
 const mostrarResultados = document.getElementById("products");
-const filtrar = document.getElementById('filtrar');
-const limpiar = document.getElementById('limpiar');
+
+//traigo la data del JSON desde la api
+async function getData(callback) {
+    try {
+    const response = await fetch(CAT_PRODUCTS)
+    const data = await response.json();
+    const products = data.products;
+    
+    
+    callback(data);
+    
+    //console.log(data);
+    //console.log(products);
+        
+    } catch (error) {
+       console.log('Error en la API: ' + error); 
+    };
+    
+};
+
+//funcion para mostrar los productos e info
+function showProductsList(data) {
+    const prod = data.products;
+    let content = `<h1 class="product-cat">${data.catName} </h1>`;
+    prod.forEach((product) => {
+        content += `
+        <div class="container">
+            <img src="${product.image}"> </img> 
+            <h2>${product.name}</h2>
+            <p>${product.description}</p>
+            <p>Precio: USD$ ${product.cost}</p>
+        </div>
+        `;
+    });
+    mostrarResultados.innerHTML = content;
+
+};
+
+getData(showProductsList);
+
+//funcion para filtrar 
+
+function filterRange (data,min,max) {
+    return data.products.filter(product => {
+        const productCost = parseInt(product.cost);
+
+        if( isNaN (min) && isNaN (max)){
+        alert ('Ingrese un monto valido');
+        return false
+
+        } if (min && max) {
+        return productCost >= min && productCost <= max;
+        }  
+        if(min){
+        return productCost >= min;
+        }  
+        if(max){
+        return productCost <= max;
+        }  {
+        return true;
+        };
+
+    });
+};
+
+
+
+// evento al boton filtrar
+const filterButton = document.getElementById('filtrar');
+filterButton.addEventListener('click', ()=>{
+    const minInput = parseInt(document.getElementById('minimo').value);
+    const maxInput = parseInt(document.getElementById('maximo').value);
+    
+    // como el fetch es una operacion async, getData se ejecuta toda y luego vuelve para ejecutar la funcion anonima con data (la que trajo de la api,ahora parametro) y pasarla a su bloque interno apra ser usada.
+    getData(function (data){
+        const listaFiltrada = filterRange(data,minInput,maxInput);
+        console.log(listaFiltrada);
+    });
+
+
+
+});
 
 
 
 
-fetch(CAT_PRODUCTS)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*fetch(CAT_PRODUCTS)
     .then((response) => {
         if (!response.ok) {
             throw new Error('Error al cargar JSON');
@@ -19,8 +125,12 @@ fetch(CAT_PRODUCTS)
         return response.json();
     })
     .then ((data) =>{
-        products = data.products;
-        let content = '';
+
+        let content = `<h1 class="product-cat">${data.catName} </h1>`;
+        const products = data.products; // data products es el array que trae el objeto
+        console.log(products); //para ver el array de productos
+        
+        //console.log(products[3].cost) // array se recorrre con indices
 
         products.forEach((product) => {
             content += `
@@ -44,109 +154,24 @@ fetch(CAT_PRODUCTS)
         });
         mostrarResultados.innerHTML = content;
 
-        getJSONData(CAT_PRODUCTS).then(function (resultObj) {
-            if (resultObj.status === "ok") {
-                const categoriesProducts = resultObj.data.products;
-                //console.log(categoriesProducts);
-
-                localStorage.setItem("listado", JSON.stringify([categoriesProducts]));
-
-            };
-        });
-    }) 
+    })
     .catch((error)=>{
         console.log(error);
-    });
-
-    //const arr = localStorage.getItem("listado");
-    let arr = localStorage.getItem("listado");
-    console.log(arr);
-
-    document.addEventListener('DOMContentLoaded', ()=>{
-
-
-    function showCategoriesList(array){
-        
-            let htmlContentToAppend = "";
-        
-            for(let i = 0; i < array.length; i++){ 
-                let product = array[i];
-                htmlContentToAppend += `
-            
-                <div class="container-products">
-                
-                <div class="product-div-img">
-                <img src="${product.image}" class="product-img">
-                </div>
-    
-                <div class="product-info">
-                <h2 class="product-name">${product.name}</h2>
-                <p class="product-descr">${product.description}</p>
-                <p class="product-price"> Precio: ${product.currency} ${product.cost}  </p>
-                <small class="text-muted">Vendidos: ${product.soldCount}</small>
-                </div>
-                
-                </div>
-                
-                `;
-        };  document.getElementById("products").innerHTML = htmlContentToAppend; 
-
-    };
-
-    //parseInt porque es un string, y necesito un integer
-    
-    
-
-    function filterRange (array){
-
-        let a = parseInt(document.getElementById('minimo').value);//tomo el valor mínimo
-        let b = parseInt(document.getElementById('maximo').value);//tomo el valor máximo
-
-        let listaFiltrada = array.filter(producto => producto.cost >= a && producto.cost <= b);
-        // arr.sort((a,b)=>a-b)
-        listaFiltrada.sort((ant,sig)=>ant.cost-sig.cost);
-
-        showCategoriesList(listaFiltrada);
-
-        //return arr.filter(producto => (producto.cost >= a && producto.cost <= b));
-
-        /*let listaFiltrada = array.filter(producto => (producto.cost >= a && producto.cost <= b));
-        // arr.sort((a,b)=>a-b)
-        listaFiltrada.sort((ant,sig)=>ant.cost-sig.cost);
-      
-        */
-    };
-
-    filtrar.addEventListener('click', ()=>{
-        filterRange(arr);
-    });
+    });*/
 
 
 
 
 
-        
-        
-    
-});
-
-    
-
-    
-
-    
-
-    
 
 
-    
 
-    
+
+
    
 
     
 
-     
 
-     
 
+   
